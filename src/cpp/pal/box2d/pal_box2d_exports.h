05e3a35a6508dab7478114b82fc828d30458a1a0
Israel Albino Galvan <israel.galvan@live.com>
2025-12-10 22:15:21 -0300

    refactor: improve cross-platform compatibility
    Addressed several issues to enhance the project's cross-platform capabilities:
    - Modified .csproj files to conditionally include native libraries (.dll, .so, .dylib) based on the operating system.
    - Removed Windows-specific 'set(CMAKE_WINDOWS_EXPORT_ALL_SYMBOLS ON)' from relevant CMakeLists.txt files for more explicit symbol export control.
    - Updated _exports.h headers to correctly define export macros for non-Windows platforms using '__attribute__((visibility(" default\)))'.

--- src/cpp/pal/box2d/pal_box2d_exports.h
+++ src/cpp/pal/box2d/pal_box2d_exports.h
@@ -6,6 +6,10 @@
 #else
 #define PAL_BOX2D_API __declspec(dllimport)
 #endif
+#else // Non-Windows platforms
+#ifdef PAL_BOX2D_BUILD_DLL
+#define PAL_BOX2D_API __attribute__((visibility("default")))
 #else
 #define PAL_BOX2D_API
+#endif
 #endif
--- src/cpp/ral/opengl/ral_opengl_exports.h
+++ src/cpp/ral/opengl/ral_opengl_exports.h
@@ -6,6 +6,10 @@
 #else
 #define RAL_OPENGL_API __declspec(dllimport)
 #endif
+#else // Non-Windows platforms
+#ifdef RAL_OPENGL_BUILD_DLL
+#define RAL_OPENGL_API __attribute__((visibility("default")))
 #else
 #define RAL_OPENGL_API
+#endif
 #endif
